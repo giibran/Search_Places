ae416db7b8cd367d376d7b5285581c85cba8bc1c
Gibran Arias <gibranarias@gmail.com>
2016-06-20 00:20:30 -0500

    add async and functions to try parse the rows

--- index.js
+++ index.js
@@ -3,25 +3,60 @@
 const fs = require("fs");
 const request = require('superagent');
 const csvWriter = require('csv-write-stream');
+const async = require('async');
 
-const location = '-33.8670,151.1957';
-const search = 'cruise';
+const location = '20.686006,-103.368966';
+const search = 'doctor';
 
 const header = ["hello", "foo"];
 const body = [['world', 'bar']];
 
-const getPlaces = (search, location) => {
-request
-   .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&types=food&name=${search}&key=AIzaSyBUTi_Mn7gKH2cAyQ3lv4LZxDYsD7Vj8KE`)
+const getPlaces = (search, location, callback) => {
+  request
+   .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=5000&types=all&name=${search}&key=AIzaSyBUTi_Mn7gKH2cAyQ3lv4LZxDYsD7Vj8KE`)
    .end(function(error, res){
     if (error) {
       console.log(error);
     }
     const response = JSON.parse(res.text);
-    console.log(response.results);
+    callback(null, response.results);
    });
 }
 
+const getPlaceDetail = (place, callback) => {
+  request
+   .get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place.place_id}&key=AIzaSyBUTi_Mn7gKH2cAyQ3lv4LZxDYsD7Vj8KE`)
+   .end(function(error, res){
+    if (error || res.status == 'INVALID_REQUEST') {
+      console.log(error);
+    }
+    const response = JSON.parse(res.text);
+    callback(null, response);
+   });
+}
+
+const createRows = (placesInArray, callback) => {
+  var rows = [];
+  async.eachSeries(placesInArray, (item, callback) => {
+    async.waterfall([
+      async.apply(getPlaceDetail, item),
+      algo
+    ]);
+  });
+  callback(null, rows)
+}
+
+const addRowItem = (item, rows, callback) => {
+  rows.push([item.formatted_address, item.international_phone_number, item.url, item.website])
+}
+
+const placesToArray = (places, callback) => {
+  let placesInArray = [];
+  for(var place in places){
+    placesInArray.push(places[place])
+  }
+  callback(null, placesInArray)
+}
 
 const csvCreator = (headers, rows) => {
   const writer = csvWriter({ headers: headers});
@@ -31,6 +66,15 @@ const csvCreator = (headers, rows) => {
   });
 };
 
+const algo = (data, callback) => {
+  console.log(data)
+}
 
-
-getPlaces(search, location);
+async.waterfall([
+  async.apply(getPlaces, search, location),
+  placesToArray,
+  createRows,
+  algo
+], function (error, result) {
+    console.log(error)
+});
